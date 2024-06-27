ad2d894730d7263a5d6b3e469625247b165f15a7
edurodrigues0 <edusacramento9@gmail.com>
2024-06-26 21:31:36 -0300

    update cv

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -5,7 +5,7 @@ import heroSvg from "public/hero.svg";
 export function Home() {
   const handleClick = () => {
     window.open(
-      "https://drive.google.com/file/d/1EJeo6wYqBBDE4-kk64-rzDFYPOOBQgTj/view?usp=sharing",
+      "https://drive.google.com/file/d/1Z2LLBOvqTbEXcwj-Td9XBz3LTvYRYiZ5/view?usp=sharing",
       "_blank",
     );
   };
